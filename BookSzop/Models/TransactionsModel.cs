38db5d585aa012a83c17404f357aeda330da3539
221049 Kusiak Juliusz <221049@edu.p.lodz.pl>
2020-12-13 18:51:22 +0100

    Added transactions listing method in TransactionsPageViewModel, created basic TransactionsPageView

---
+++ BookSzop/Models/TransactionsModel.cs
@@ -0,0 +1,13 @@
+﻿using ShopService.Models.EventModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BookSzop.Models
+{
+    class TransactionsModel
+    {
+        public ObservableCollection<IEvent> Transactions { get; } = new ObservableCollection<IEvent>();
+    }
+}
--- BookSzop/ViewModels/TransactionPageViewModel.cs
+++ BookSzop/ViewModels/TransactionPageViewModel.cs
@@ -1,8 +1,15 @@
 ﻿using BookSzop.Commands;
+using BookSzop.Models;
+using BookSzop.Models.PagesModels;
 using BookSzop.Utils;
 using BookSzop.ViewModels.Base;
+using ShopService.Models.EventModel;
+using ShopService.Purchase;
+using ShopService.StoreManagement;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 
@@ -10,6 +17,14 @@ namespace BookSzop.ViewModels
 {
     public class TransactionPageViewModel : ViewModelBase
     {
+        private IPurchaseService _purchaseService { get; }
+        private IStoreManagementService _storeManagementService { get; }
+        private TransactionsModel _transactionsModel { get; }
+        public ObservableCollection<IEvent> Transactions { get => _transactionsModel.Transactions; }
+
+
+
+
         public ICommand BackCommand
         {
             get => new RelayCommand(param =>
@@ -18,5 +33,30 @@ namespace BookSzop.ViewModels
                 NavigationHelper.NavigationService.GoBack();
             });
         }
+
+        public TransactionPageViewModel(IPurchaseService purchaseService, IStoreManagementService storeManagementService)
+        {
+            SessionHelper.SessionChanged += UpdateTransactionData;
+            _purchaseService = purchaseService;
+            _storeManagementService = storeManagementService;
+            _transactionsModel = new TransactionsModel();
+
+        }
+
+
+        public void UpdateTransactionData(object sender, EventArgs e)
+        {
+            int? userId = SessionHelper.GetSessionUserId();
+
+            _transactionsModel.Transactions.Clear();
+
+            _purchaseService
+                .GetUserPurchases((int)userId)
+                .ToList()
+                .ForEach(purchase => _transactionsModel.Transactions.Add(purchase));
+
+        }
+
+
     }
 }
